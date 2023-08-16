6f9a4e48bfade46f1e2353c3cb242b3ca263abf7
pratyush <pratyushpathak0208@gmail.com>
2023-08-16 05:49:23 -0700

    analysis

---
+++ streamlit.py
@@ -0,0 +1,6 @@
+import streamlit as st
+st.title("Hello :wave:")
+st.divider()
+st.write("Streamlit is very _fast_ to build and share **data apps**")
+st.divider()
+st.markdown("Contact me on [linkedin](https://www.linkedin.com/in/pratyush-pathak/)")
